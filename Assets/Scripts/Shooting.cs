15ffa701f74da59ca271068d9f8317278eefcb4c
kingkaue <kaueking@gmail.com>
2025-01-22 23:18:46 -0500

    Added shooting mechanic to player

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -2,18 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Shooting : MonoBehaviour
 {
-
-    public float moveSpeed = 5;
-    public float hInput;
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
     public float bulletSpeed = 10;
-    public float fireRate = 0.2f;
-    private float nextFireTime = 0f;
 
-    // Start is called before the first frame update
     void Start()
     {
         if (bulletPrefab != null && GetComponentInParent<Collider2D>() != null)
@@ -22,24 +16,13 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update ()
     {
-        hInput = Input.GetAxisRaw("Horizontal");
-
-        transform.Translate(Vector2.right * hInput * moveSpeed * Time.deltaTime);
-
-        if (Input.GetKey(KeyCode.Space) && Time.time >= nextFireTime)
+        if (Input.GetKey(KeyCode.Space))
         {
-            Shoot();
-            nextFireTime = Time.time + fireRate;
-        }
-    }
-
-    void Shoot()
-    {
             var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
             bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.up * bulletSpeed;
             Debug.Log("Shooting");
+        }
     }
 }
